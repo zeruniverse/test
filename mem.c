860c2a5176155ac081d030556c90ab446693d583
Jeffery Zhao <zzy8200@gmail.com>
2015-11-09 17:25:38 -0800

    Update mem.c

--- mem.c
+++ mem.c
@@ -8,20 +8,6 @@ static ssize_t read_mem(struct file *file, char __user *buf,
 	if (!valid_phys_addr_range(p, count))
 		return -EFAULT;
 	read = 0;
-#ifdef __ARCH_HAS_NO_PAGE_ZERO_MAPPED
-	/* we don't have page 0 mapped on sparc and m68k.. */
-	if (p < PAGE_SIZE) {
-		sz = size_inside_page(p, count);
-		if (sz > 0) {
-			if (clear_user(buf, sz))
-				return -EFAULT;
-			buf += sz;
-			p += sz;
-			count -= sz;
-			read += sz;
-		}
-	}
-#endif
 
 	while (count > 0) {
 		unsigned long remaining;
