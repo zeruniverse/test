1011dc23b54c14d6591918a233955b9ca33ed341
Jeffery Zhao <zzy8200@gmail.com>
2015-11-18 22:46:29 -0800

    Delete config.h

--- config.h
+++
@@ -1,9 +0,0 @@
-while true
-do
-   if acpi -a | grep off-line > /dev/null; then
-       xsetroot -name "Bat. $( acpi -b | awk '{ print $4 " " $5 }' | tr -d ',' ) | Vol. $(amixer get Master | tail -1 | awk '{ print $5}' | tr -d '[]') | $(date +"%a, %b %d %R")"
-   else
-       xsetroot -name "Vol. $(amixer get Master | tail -1 | awk '{ print $5}' | tr -d '[]') | $(date +"%a, %b %d %R")"
-   fi
-   sleep 1s   
-done &
